e86d2732d8b9ebb218c5ebcc6384fb9cc6c6f209
German Solano <german@famosos.com>
2022-01-13 11:15:01 -0400

    feat(<HomePageLayout />): :sparkles: Add elements and styles for mobile

--- components/pages/home/index.jsx
+++ components/pages/home/index.jsx
@@ -1,4 +1,6 @@
+import { ComeAndBeInspired } from "@/components/home/come-and-be-inspired";
 import { Hero } from "@/components/home/hero";
+import { YourDay } from "@/components/home/your-day";
 import { PageLayout } from "@/components/layouts/page-layout";
 
 function HomePageLayout() {
@@ -6,6 +8,8 @@ function HomePageLayout() {
     <PageLayout showHeader={false}>
       <main>
         <Hero />
+        <YourDay className="pt-40 pb-8" />
+        <ComeAndBeInspired className="pb-40" />
       </main>
     </PageLayout>
   );
