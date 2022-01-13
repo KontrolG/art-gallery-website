150869f51227f2495f502c24b914211d15a261fe
German Solano <german@famosos.com>
2022-01-13 10:38:47 -0400

    feat(<CallToActionLink />): :lipstick: Add alternative variant styles

--- components/common/widget/call-to-action-link/index.jsx
+++ components/common/widget/call-to-action-link/index.jsx
@@ -2,17 +2,31 @@ import { Link } from "@/components/common/routing/link";
 import { SHARED_IMAGES_PATH } from "@/constants/paths";
 import classes from "classnames";
 
-function CallToActionLink({ className, href, children, linkProps }) {
+function CallToActionLink({ className, href, children, linkProps, variant }) {
   return (
     <Link
       className={classes("inline-flex group text-white", className)}
       {...linkProps}
       href={href}
     >
-      <div className="bg-black py-[1em] px-[1.5em] uppercase font-serif flex-centered group-hover:bg-emphasis transition-colors">
+      <div
+        className={classes(
+          " py-[1em] px-[1.5em] uppercase font-serif flex-centered transition-colors",
+          variant === "alternative"
+            ? "bg-emphasis group-hover:bg-black"
+            : "bg-black group-hover:bg-emphasis"
+        )}
+      >
         {children}
       </div>
-      <div className="p-[1em] bg-emphasis flex-centered group-hover:bg-black transition-colors">
+      <div
+        className={classes(
+          "p-[1em] flex-centered transition-colors",
+          variant === "alternative"
+            ? "bg-black group-hover:bg-emphasis"
+            : "bg-emphasis group-hover:bg-black"
+        )}
+      >
         <img
           className="h-[1em] w-auto"
           src={SHARED_IMAGES_PATH + "/icon-arrow-right.svg"}
