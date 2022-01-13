839400d9442ca96a72635045ce5be8036d3a77d4
German Solano <german@famosos.com>
2022-01-13 11:14:23 -0400

    feat(<PageLayout />): :sparkles: Add <Footer />

--- components/layouts/page-layout/index.jsx
+++ components/layouts/page-layout/index.jsx
@@ -1,13 +1,17 @@
 import { Header } from "../header";
 import Maybe from "@/components/common/helpers/maybe";
+import { Footer } from "../footer";
 
-function PageLayout({ children, showHeader = true }) {
+function PageLayout({ children, showHeader = true, showFooter = true }) {
   return (
     <>
       <Maybe it={showHeader}>
         <Header />
       </Maybe>
       {children}
+      <Maybe it={showFooter}>
+        <Footer />
+      </Maybe>
     </>
   );
 }
