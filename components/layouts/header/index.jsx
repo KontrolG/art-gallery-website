c8916985e14ba125273151f8e7ac2db63a9dde1d
German Solano <german@famosos.com>
2022-01-14 08:15:16 -0400

    feat(<Header />): :sparkles: Add elements and styles

--- components/layouts/header/index.jsx
+++ components/layouts/header/index.jsx
@@ -1,17 +1,16 @@
-import { Link } from "@/components/common/routing/link";
-import { HOME_PATH, SHARED_IMAGES_PATH } from "@/constants/paths";
+import { CallToActionLink } from "@/components/common/widget/call-to-action-link";
+import { HOME_PATH } from "@/constants/paths";
 
 function Header() {
   return (
-    <header className="container xl:max-w-7xl pt-6 lg:pt-10 flex justify-between items-center z-10">
-      <Link href={HOME_PATH}>
-        <img
-          src={SHARED_IMAGES_PATH + "/logo.svg"}
-          alt="Space Tourism"
-          className="w-10 h-10 md:w-12 md:h-12"
-        />
-      </Link>
-      <hr className="hidden xl:block absolute border-white ml-28 2xl:ml-48 border-opacity-25 w-[32.8472222vw] z-20" />
+    <header className="container z-10 relative">
+      <CallToActionLink
+        href={HOME_PATH}
+        direction="left"
+        className="text-sm absolute"
+      >
+        Back to home
+      </CallToActionLink>
     </header>
   );
 }
